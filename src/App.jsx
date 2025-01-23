12e35024625831ef3d015bcf78520afebd9a767b
WodaMineralna <raknieborak65@gmail.com>
2025-01-23 16:52:17 +0100

    test #2

--- src/App.jsx
+++ src/App.jsx
@@ -51,7 +51,7 @@ function App() {
         <h1>PlacePicker</h1>
         <p>
           Create your personal collection of places you would like to visit or
-          you have visited. test
+          you have visited. test TEST2
         </p>
       </header>
       <main>
